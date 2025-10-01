fb1c8d4c1483c2727da80b70ef4d46f811bc5d21
Koil <99706942+KoilyKoil@users.noreply.github.com>
2025-10-01 16:42:59 +0900

    공연장 씬 연결
    - 공연장 씬(Session_1-2)추가 및 연결 처리
       - 씬 이동은 'NextGate' 오브젝트에서 담당
       - 은색 열쇠 아이템을 먹어야만 이동 가능
       - 씬 이동만 구현하고, 별도의 맵 배치는 진행하지 않은 상태
    - 공연장씬에서도 공통적으로 사용될 오브젝트 (UI, 플레이어 등) 재배치 및 동기화
       - 아이템 테스트도 확인했으므로, 타일맵과 아이템만 배치하면 되는 상태

---
+++ Assets/Assets/Resources/Script/Door_SceneMover.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door_SceneMover : MonoBehaviour
+{
+    //씬 이동을 처리하는 코드드
+    //플레이어의 소지 아이템 상태에 따라 잠김 상태 구현
+    //컴포넌트
+    [SerializeField] private Inventory inventory;
+
+    //접근 변수
+    [SerializeField] private int itemCode;          //문을 여는데 필요한 아이템 코드
+    [SerializeField] private string sceneTarget;    //이동하고자 하는 목적지 씬 이름
+
+    //비접근 변수
+    bool isMovable = false;         //이동 가능한 상태인지 확인하기 위한 변수
+
+    //함수
+    void Update()
+    {
+        //완전 개방 문이거나        특정 아이템을 소지한 상태인 경우 열림처리 활성화
+        if (isMovable == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneTarget);
+        }
+    }
+
+
+    //콜라이더 접근 상태에 따라 이동 가능상태 설정
+    void OnTriggerEnter2D(Collider2D obj) {
+        if (obj.gameObject.CompareTag("Player") == true && inventory.SearchItem(itemCode) == true) isMovable = true;
+        else isMovable = false;
+    }
+    
+    void OnTriggerExit2D(Collider2D obj) {
+        isMovable = false;
+    }
+}
